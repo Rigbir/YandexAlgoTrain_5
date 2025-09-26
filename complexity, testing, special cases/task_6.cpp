42b28062a408f202c7f374718eb0c21515c3e86c
Marat <m.brezin@yandex.ru>
2025-09-26 21:58:41 +0300

    add 6 task

--- complexity, testing, special cases/task_6.cpp
+++ complexity, testing, special cases/task_6.cpp
@@ -1,3 +1,37 @@
 //
 // Created by Marat on 25.09.25.
 //
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v[i];
+    }
+
+    std::vector<char> signs;
+    int prev = std::abs(v[0]) % 2;
+    for (int i = 1; i < n; ++i) {
+        int curr = std::abs(v[i]) % 2;
+        if (curr == 1) {
+            if (prev == 1) {
+                signs.push_back('x');
+                prev = 1;
+            } else {
+                signs.push_back('+');
+                prev = 1;
+            }
+        } else {
+            signs.push_back('+');
+        }
+    }
+
+    for (auto c : signs) {
+        std::cout << c;
+    }
+}
