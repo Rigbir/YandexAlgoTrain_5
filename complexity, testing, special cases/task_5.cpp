e82108e715216feabd9dbfed36349e54a4276f2d
Marat <m.brezin@yandex.ru>
2025-09-26 04:48:40 +0300

    add 5 task

--- complexity, testing, special cases/task_5.cpp
+++ complexity, testing, special cases/task_5.cpp
@@ -1,3 +1,32 @@
 //
 // Created by Marat on 25.09.25.
 //
+
+#include <iostream>
+#include <string>
+
+int main() {
+    long long n, k, d;
+    std::cin >> n >> k >> d;
+
+    std::string output = std::to_string(n);
+    long long mod = n % k;
+
+    for (int i = 0; i < d; ++i) {
+        bool found = false;
+        for (int j = 0; j <= 9; ++j) {
+            if ((mod * 10 + j) % k == 0) {
+                output.push_back('0' + j);
+                mod = 0;
+                found = true;
+                break;
+            }
+        }
+        if (!found) {
+            std::cout << -1;
+            return 0;
+        }
+    }
+
+    std::cout << output;
+}
