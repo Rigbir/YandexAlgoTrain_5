f6bee0352b5432e25e4bd91e4dbbf75fb7a3b5f0
Marat <m.brezin@yandex.ru>
2025-09-26 03:10:29 +0300

    add 3 task

--- complexity, testing, special cases/task_3.cpp
+++ complexity, testing, special cases/task_3.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Marat on 25.09.25.
 //
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<long long> v(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v[i];
+    }
+
+    long long count = 0;
+    for (size_t i = 0; i < v.size(); ++i) {
+        long long tabs = v[i] / 4;
+        long long rem = v[i] % 4;
+        count += tabs + std::min(rem, 5 - rem);
+    }
+
+    std::cout << count;
+}
