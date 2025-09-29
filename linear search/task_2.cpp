de6fa39230eb2b7be329d257df02784ec47c4d2e
Marat <m.brezin@yandex.ru>
2025-09-29 04:37:08 +0300

    add 2 task

--- linear search/task_2.cpp
+++ linear search/task_2.cpp
@@ -1,3 +1,26 @@
 //
 // Created by Marat on 29.09.25.
 //
+
+#include <iostream>
+#include <vector>
+#include <climits>
+
+int main() {
+    int n, k;
+    std::cin >> n >> k;
+
+    std::vector<int> v(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v[i];
+    }
+
+    int cost = 0;
+    for (int day = 0; day < n; ++day) {
+        for (int sell = day + 1; sell <= std::min(n - 1, day + k); ++sell) {
+            cost = std::max(cost, v[sell] - v[day]);
+        }
+    }
+
+    std::cout << cost;
+}
