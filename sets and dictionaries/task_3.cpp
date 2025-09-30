13e83d48dd890f44e1ca22fdd0e0b571c5f1acf6
Marat <m.brezin@yandex.ru>
2025-09-30 15:23:25 +0300

    add 3 task

--- sets and dictionaries/task_3.cpp
+++ sets and dictionaries/task_3.cpp
@@ -1,3 +1,25 @@
 //
 // Created by Marat on 30.09.25.
 //
+
+#include <iostream>
+#include <vector>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> freq(100001 + 1, 0);
+    for (int i = 0; i < n; ++i) {
+        int x;
+        std::cin >> x;
+        ++freq[x];
+    }
+
+    int countDelete = 0;
+    for (int i = 0; i <= 100000; ++i) {
+        countDelete = std::max(countDelete, freq[i] + freq[i + 1]);
+    }
+
+    std::cout << n - countDelete;
+}
