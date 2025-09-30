a6b5e04793bd1a5d52d7bfff3a657a33e7d5b538
Marat <m.brezin@yandex.ru>
2025-09-30 03:59:23 +0300

    add 9 task

--- linear search/task_9.cpp
+++ linear search/task_9.cpp
@@ -1,3 +1,30 @@
 //
 // Created by Marat on 29.09.25.
 //
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<std::pair<int, int>> v(n);
+    for (int i = 0; i < n; ++i) {
+        std::cin >> v[i].first >> v[i].second;
+    }
+
+    std::sort(v.begin(), v.end(), [](auto &a, auto &b){ return a.first < b.first; });
+
+    int ans = 1e9;
+    for (int col = 1; col < n + 1; ++col) {
+        int now = 0;
+        for (int i = 0; i < n; ++i) {
+            now += std::abs(v[i].first - (i + 1)) + std::abs(v[i].second - col);
+        }
+        ans = std::min(ans, now);
+    }
+
+    std::cout << ans;
+}
