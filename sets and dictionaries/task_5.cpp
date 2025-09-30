682a7cce78fbad105c44ebaf863571ff7477606c
Marat <m.brezin@yandex.ru>
2025-10-01 00:25:21 +0300

    add 5 task

--- sets and dictionaries/task_5.cpp
+++ sets and dictionaries/task_5.cpp
@@ -1,3 +1,39 @@
 //
 // Created by Marat on 30.09.25.
 //
+
+#include <iostream>
+#include <set>
+#include <unordered_map>
+#include <unordered_set>
+
+int main() {
+
+    std::unordered_map<int, int> freq;
+    for (int i = 0; i < 3; ++i) {
+        int n;
+        std::cin >> n;
+
+        std::unordered_set<int> curr;
+        for (int j = 0; j < n; ++j) {
+            int x;
+            std::cin >> x;
+            curr.insert(x);
+        }
+
+        for (auto s : curr) {
+            ++freq[s];
+        }
+    }
+
+    std::set<int> output;
+    for (auto &[x, cnt] : freq) {
+        if (cnt >= 2) {
+            output.insert(x);
+        }
+    }
+
+    for (auto i : output) {
+        std::cout << i << ' ';
+    }
+}
