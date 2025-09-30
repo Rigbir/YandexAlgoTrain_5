5a5e55914c81585846dc48867c46bbd17110bd20
Marat <m.brezin@yandex.ru>
2025-10-01 00:55:27 +0300

    add 6 task

--- sets and dictionaries/task_6.cpp
+++ sets and dictionaries/task_6.cpp
@@ -1,3 +1,47 @@
 //
 // Created by Marat on 30.09.25.
 //
+
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+
+int main() {
+    std::string wordInDict;
+    std::getline(std::cin, wordInDict);
+
+    std::vector<std::string> wordsInDictionary;
+    {
+        std::istringstream iss(wordInDict);
+        std::string word;
+        while (iss >> word) {
+            wordsInDictionary.push_back(word);
+        }
+    }
+
+    std::sort(wordsInDictionary.begin(), wordsInDictionary.end(),
+            [](const std::string& s1, const std::string& s2) {
+                return s1.size() < s2.size();
+            });
+
+    std::string text;
+    std::getline(std::cin, text);
+
+    std::istringstream iss(text);
+    std::string word;
+    bool first = true;
+    while (iss >> word) {
+        std::string replaced = word;
+        for (const auto& pref : wordsInDictionary) {
+            if (word.rfind(pref, 0) == 0) {
+                replaced = pref;
+                break;
+            }
+        }
+        if (!first) std::cout << " ";
+        std::cout << replaced;
+        first = false;
+    }
+}
